afc50ec5d49d48445780f0cc002d6dff5d0b1c86
github username <josephtrettevik@gmail.com>
2019-08-21 16:45:00 -0700

    Moved all the methods from command_line_interface.rb to run.rb.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,42 +1,42 @@
-require_relative '../config/environment'
+# require_relative '../config/environment'
 
-def welcome
-    puts "Welcome to Breweries Go, where you can find and catch your favorite breweries."
-end
+# def welcome
+#     puts "Welcome to Breweries Go, where you can find and catch your favorite breweries."
+# end
   
-def get_login
-  puts "Please select an option:"
-  puts "1. Login with your Username."
-  puts "2. Create a User Profile."
+# def get_login
+#   puts "Please select an option:"
+#   puts "1. Login with your Username."
+#   puts "2. Create a User Profile."
 
-  # use gets to capture the user's input. 
-  option = gets.chomp
-  option
-end
+#   # use gets to capture the user's input. 
+#   option = gets.chomp
+#   option
+# end
 
-def prompt_user_creation
-  User.create(name: get_username)
-end
+# def prompt_user_creation
+#   User.create(name: get_username)
+# end
 
-def get_user_profile
-  User.find_by(name: get_username)
-end
+# def get_user_profile
+#   User.find_by(name: get_username)
+# end
 
-def get_username
-  puts "Please a username and enter it into the console."
-  # binding.pry
-  username = gets.chomp
-  username
-end
+# def get_username
+#   puts "Please a username and enter it into the console."
+#   # binding.pry
+#   username = gets.chomp
+#   username
+# end
 
-def get_menu_option
-  puts "Please select an option:"
-  puts "1. Search for a Breweries by city."
-  puts "2. See your list of Breweries."
+# def get_menu_option
+#   puts "Please select an option:"
+#   puts "1. Search for a Breweries by city."
+#   puts "2. See your list of Breweries."
     
-  # use gets to capture the user's input.
-  option = gets.chomp
-  return option
-end
+#   # use gets to capture the user's input.
+#   option = gets.chomp
+#   return option
+# end
 
   
